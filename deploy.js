2036c726679e4bdb772ec1ea96f757807e2128cf
Justin McLaren <justinmclaren63@gmail.com>
2018-03-22 15:19:34 -0700

    deployed to rinkeby test network

--- deploy.js
+++ deploy.js
@@ -6,3 +6,18 @@ const provider = new HDWalletProvider(
   'blur hope animal glare argue loyal alcohol envelope brand arctic toast upset',
   'https://rinkeby.infura.io/VWis7vNnjNF3qqCa4v1T'
 );
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+  // interface is the ABI 
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
